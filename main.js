ba69ef1ab8b688fb78888e8f26634308d2ef03c7
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-06-20 13:39:38 +0530

    this is a failure

--- main.js
+++ main.js
@@ -4,22 +4,21 @@
 // import tf from "@tensorflow/tfjs";
 import fetch from "node-fetch";
 async function run() {
-	const questions = {
-		questions: [
-			"what is the capital of india?",
-			"what is the capital of china?",
-		]
-	}
-			
+	const questions = [
+		"what is the capital of india?",
+		"what is the capital of china?",
+	];
+
 	const res = await fetch(
 		"https://questionsclassifier.azurewebsites.net/predict",
 		{
 			method: "POST",
+			headers: { "content-type": "application/json" },
 			body: JSON.stringify(questions),
 		}
 	);
-	// const data = await res.json();
-	// console.log(data);
+	const data = await res.json();
+	console.log(data);
 }
 
 run();
--- test.py
+++ test.py
@@ -1,3 +1,22 @@
 from functions import predict
+import json
+import html
 
-print(predict('What is the weather today?'))
+filef = open("questions.json", "r")
+data = json.load(filef)['results']
+filef.close()
+
+totalQuestions = len(data)
+correct = 0
+for i in range(totalQuestions):
+    question = html.unescape(data[i]['question'])
+    difficulty = data[i]['difficulty']
+    prediction = predict(question)
+    if prediction == difficulty:
+        correct += 1
+    else:
+        print(question)
+        print(difficulty)
+        print(prediction)
+        print()
+print(correct / totalQuestions)
